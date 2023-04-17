a5f7c8fcd41deff51ac6954ca31a7c4eda0dceb7
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 09:42:06 +0200

    Created Event class  - added base Properties structure  - added empty Constructor

---
+++ Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Event
+    {
+
+        //EVENT's PROPERTIES
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public int Capacity { get; }
+        public int Booked { get; }
+
+        //EVENT's CONSTRUCTOR
+        internal Event()
+        {
+
+        }
+
+        //EVENT's METHODS
+        //...
+    }
+}
