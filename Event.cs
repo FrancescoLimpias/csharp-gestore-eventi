2fafff86c2196241aa8e6a0a0a95d00a90a2acdc
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 10:07:42 +0200

    Added Properties validations  + added base constructor

--- Event.cs
+++ Event.cs
@@ -9,16 +9,66 @@ namespace GestoreEventi
     internal class Event
     {
 
-        //EVENT's PROPERTIES
-        public string Title { get; set; }
-        public DateTime Date { get; set; }
-        public int Capacity { get; }
+        /* *******************
+         * EVENT's PROPERTIES
+         */
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                //check if Title is null or empty
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException($"The given Title is empty! \"{value}\"");
+                _title = value.Trim();
+            }
+        }
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                //check if Date has passed
+                if (DateTime.Now > value)
+                    throw new ArgumentException($"The given Date is passed! \"{value}\"");
+                _date = value;
+            }
+        }
+
+        private int _capacity;
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                //check if Capacity is positive
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException($"The given Capacity is 0 or negative! \"{value}\"");
+                _capacity = value;
+            }
+        }
+
         public int Booked { get; }
 
         //EVENT's CONSTRUCTOR
-        internal Event()
+        internal Event(string title, DateTime date, int capacity)
         {
-
+            Title = title;
+            Date = date;
+            Capacity = capacity;
+            Booked = 0;
         }
 
         //EVENT's METHODS
