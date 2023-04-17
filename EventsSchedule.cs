50e84792232fcc67defbec441663bdf5c4c428af
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 12:57:58 +0200

    Added EventsSchedule methods

--- EventsSchedule.cs
+++ EventsSchedule.cs
@@ -14,7 +14,8 @@ namespace GestoreEventi
         public string Title { get; set; }
 
         private List<Event> _events;
-        public List<Event> Events {
+        public List<Event> Events
+        {
             get
             {
                 return new List<Event>(_events);
@@ -24,11 +25,41 @@ namespace GestoreEventi
                 _events = value;
             }
         }
+        public int NumberOfEvents => _events.Count;
 
         //EVENTsSCHEDULE's CONSTRUCTOR
-        internal EventsSchedule(string title) {
+        internal EventsSchedule(string title)
+        {
             Title = title;
             Events = new List<Event>();
+        }
+
+        /* *******************
+         * EVENTsSCHEDULE's METHODS
+         */
+        public void Add(Event e)
+        {
+            _events.Add(e);
+        }
+        public List<Event> GetEventsByDate(DateTime date)
+        {
+            return Events.FindAll(e => e
+                    ./*event date property*/Date
+                    ./*date value inside datetime object*/Date 
+                    == date.Date);
+        }
+        public static string EventsToString(List<Event> events)
+        {
+            return string.Join("\r\n", events.Select(e => e.ToString()));
+        }
+        public void ClearEvents()
+        {
+            _events.Clear();
+        }
+        public override string ToString()
+        {
+            return $"{Title}:\r\n"
+                + EventsToString(_events);
         }
     }
 }
