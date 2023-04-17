04058546d3e4ba29563770c7cd05b5bf47311975
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 14:21:38 +0200

    Added Testing for EventsSchedule

--- Program.cs
+++ Program.cs
@@ -4,13 +4,78 @@
     {
         static void Main(string[] args)
         {
+
             //Greet the user
             Console.WriteLine();
-            Console.WriteLine("Welcome to EventsHandler!");
+            Console.WriteLine("\tWelcome to EventsHandler!");
             Console.WriteLine();
 
-            //Ask to insert an event
-            Console.WriteLine("Create an Event");
+            //Create an EventsSchedule
+            Console.WriteLine("EVENTS SCHEDULE");
+
+            //Ask for its title
+            Console.Write("Please, insert a Title: ");
+            string scheduleTitle = UConsole.AskString();
+
+            //Ask for the number of hosted events
+            Console.Write("Please, insert a Number of Events: ");
+            int scheduleNumberOfEvents = UConsole.AskInt();
+
+            //Instantiate EventsSchedule
+            EventsSchedule ES = new EventsSchedule(scheduleTitle);
+
+            //Loop until the desired amount of events it's reached
+            while (ES.NumberOfEvents < scheduleNumberOfEvents)
+            {
+                Console.WriteLine();
+
+                Event newEvent;
+                try
+                {
+                    newEvent = AskToCreateEvent();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"ERR - {e.Message}");
+                    continue;
+                }
+
+                //Finally add the event to the schedule
+                ES.Add(newEvent);
+            }
+
+            /* ***********
+             * PRINTING
+             */
+            //details
+            Console.WriteLine();
+            Console.WriteLine("OVERVIEW");
+            Console.WriteLine($"Number of events: {ES.NumberOfEvents}");
+            Console.WriteLine(ES.ToString());
+            Console.WriteLine();
+
+            //date select
+            Console.WriteLine("DATE SELECTED EVENTS");
+            Console.Write("Please, insert a Date of Events to print (dd/MM/yyyy): ");
+            DateTime dateToSelect = UConsole.AskStringToCast((input) => DateTime.Parse(input + " 12:00:00"));
+            Console.WriteLine(EventsSchedule.EventsToString(ES.GetEventsByDate(dateToSelect)));
+
+            //empty
+            Console.WriteLine();
+            ES.ClearEvents();
+            Console.WriteLine("Cleared Events");
+
+            //goodbye
+            Console.WriteLine();
+            Console.WriteLine("Bye");
+
+        }
+
+        internal static Event AskToCreateEvent()
+        {
+
+            //Insert an event
+            Console.WriteLine("EVENT");
 
             //ASk for its title
             Console.Write("Please, insert a Title: ");
@@ -28,7 +93,9 @@
             Event newEvent = new Event(eventName, eventDate, eventCapacity);
             Console.WriteLine();
             Console.WriteLine($"Event created: {newEvent.ToString()}");
+            Console.WriteLine();
 
+            /* THIS WAS FOR MILESTONE 2
             //Ask for booked seats
             Console.Write("Please, insert booked seats: ");
             newEvent.BookSeats(UConsole.AskInt());
@@ -62,7 +129,9 @@
                 }
 
             } while (cancel);
+            */
 
+            return newEvent;
         }
     }
 }
