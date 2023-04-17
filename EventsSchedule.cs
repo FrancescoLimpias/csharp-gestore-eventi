69082fbccfa801df52d7ea363fd7d2478c8598ad
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 12:44:23 +0200

    Created EventsSchedule class  - added base properties  - added base constructor

---
+++ EventsSchedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class EventsSchedule
+    {
+        /* *******************
+         * EVENTsSCHEDULE's PROPERTIES
+         */
+        public string Title { get; set; }
+
+        private List<Event> _events;
+        public List<Event> Events {
+            get
+            {
+                return new List<Event>(_events);
+            }
+            private set
+            {
+                _events = value;
+            }
+        }
+
+        //EVENTsSCHEDULE's CONSTRUCTOR
+        internal EventsSchedule(string title) {
+            Title = title;
+            Events = new List<Event>();
+        }
+    }
+}
