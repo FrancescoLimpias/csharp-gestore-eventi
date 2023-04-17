0c3de900e920f0ae5a84484c3d6424201ad3b237
Francesco Limpias <francescolimpias@gmail.com>
2023-04-17 11:50:09 +0200

    Added utility class "UConsole"  - added AskString & AskStringToCast methods that allow for easy console input handling  - added stacked locking mechanism for Cursor locking

---
+++ UConsole.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    /* UTILITIES for CONSOLE
+     * 
+     */
+    internal static class UConsole
+    {
+        /* CURSOR LOCKING MECHANISM
+         * (private, it's an internal mechanic)
+         */
+        private static int lockStack = 0;
+        private static (int left, int top)? lockedCursorPosition = null;
+
+        private static bool IsCursorLocked()
+        {
+            return lockedCursorPosition != null;
+        }
+        private static void LockCursor()
+        {
+            if (lockStack++ == 0)
+            {
+                lockedCursorPosition = Console.GetCursorPosition();
+            }
+        }
+        private static void UnlockCursor()
+        {
+            if (--lockStack == 0)
+            {
+                lockedCursorPosition = null;
+            }
+        }
+        private static void ResumeCursorPosition()
+        {
+            Console.SetCursorPosition(lockedCursorPosition.Value.left, lockedCursorPosition.Value.top);
+            Console.Write(new string(' ', 10));
+            Console.SetCursorPosition(lockedCursorPosition.Value.left, lockedCursorPosition.Value.top);
+        }
+
+        //ASK FOR INPUT STRING
+        internal static string AskString()
+        {
+            //Lock cursor
+            LockCursor();
+            ResumeCursorPosition();
+
+            //Ask for string and check input
+            string? input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+                return AskString();
+
+            //Unlock cursor and return
+            UnlockCursor();
+            return input;
+        }
+
+        //ASK FOR INPUT STRING AND CAST
+        internal static T AskStringToCast<T>(Func<string, T> CastingMethod)
+        {
+            //Lock cursor
+            LockCursor();
+
+            string input = AskString();
+            T result;
+            try
+            {
+                result = CastingMethod(input);
+            }
+            catch (Exception)
+            {
+                result = AskStringToCast(CastingMethod);
+            }
+
+            UnlockCursor();
+            return result;
+        }
+    }
+}
